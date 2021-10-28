8680e96877df4296346ea39ac6a86ae1f2cb12d8
Elena <elena@manele.se>
2021-10-28 23:09:53 +0200

    Copy files instead of linking

--- test/patches/arduino_mkrrgb/src/MKRRGBMatrix.h
+++ test/patches/arduino_mkrrgb/src/MKRRGBMatrix.h
@@ -1,1 +1,50 @@
-../../../../extra/samples/mkrrgb/library_patches/arduino_mkrrgb/src/MKRRGBMatrix.h
+/*
+  This file is part of the Arduino_MKRRGB library.
+  Copyright (c) 2019 Arduino SA. All rights reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef _MKR_RGB_MATRIX_H
+#define _MKR_RGB_MATRIX_H
+
+#include <MkrRgb.h>
+#include "../../../ArduinoGraphics/ArduinoGraphics-1.0.0/src/ArduinoGraphics.h"
+
+#define RGB_MATRIX_WIDTH 12
+#define RGB_MATRIX_HEIGHT 7
+
+class RGBMatrixClass : public ArduinoGraphics {
+  public:
+    RGBMatrixClass();
+    virtual ~RGBMatrixClass();
+
+    int begin();
+    void end();
+
+    void brightness(uint8_t brightness);
+
+    virtual void beginDraw();
+    virtual void endDraw();
+
+    virtual void set(int x, int y, uint8_t r, uint8_t g, uint8_t b);
+
+  private:
+    MkrRgb inner;
+};
+
+extern "C" RGBMatrixClass MATRIX;
+
+#endif
