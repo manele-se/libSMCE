55d722425b7a1ba84947fd05d4c8a56442fd47b0
Wei Guo <guow1227@gmail.com>
2021-11-10 18:16:54 +0100

    try again

--- ci/setup-cmake-interceptor.sh
+++ ci/setup-cmake-interceptor.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 base="${GITHUB_WORKSPACE}"
 cmake_bindir="$(dirname "$(which cmake)")"
+CODECOV_TOKEN='c4bbabdf-38be-4bef-8635-0e2cc248ca3f'
 
 mv "${cmake_bindir}/cmake" "${cmake_bindir}/cmake-real"
 sed "s@INJECTION-POINT@cmake_bindir=\"${cmake_bindir}\"@" "${base}/ci/cmake-interceptor.sh" > "${cmake_bindir}/cmake"
