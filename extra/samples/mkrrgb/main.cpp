3fb6bafdbcf5dee05908737e55adefcfd47fcbca
Elena <elena@manele.se>
2021-11-12 22:45:38 +0100

    Work in progress, getting MKRRGB sample to work

--- extra/samples/mkrrgb/main.cpp
+++ extra/samples/mkrrgb/main.cpp
@@ -63,6 +63,8 @@ int main(int argc, char** argv) {
             .name = "ArduinoGraphics",
             .version = "1.0.0",
             .uri = "https://github.com/arduino-libraries/ArduinoGraphics/archive/refs/tags/1.0.0.tar.gz",
+            .patch_uri = "file://" + (std::filesystem::current_path() /
+                                      "library_patches" / "arduino_graphics").generic_string(),
             .defaults = smce::PluginManifest::Defaults::arduino
         },
         smce::PluginManifest{
